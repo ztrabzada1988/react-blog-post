f862e5b85f6a9e14b59d6bcfc693d7750b36a4f2
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-12-13 22:41:19 -0800

    added functionality to fetch API data

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,14 +1,46 @@
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+import _ from 'lodash';
 
-class PostIndex extends Component {
+class PostsIndex extends Component {
+
+    // Lifecycle methods are immediately run by react once this component
+    // shows up in the DOM which is great to use for times when we call 
+    // to fetch data without an event being triggered like the fetching API 
+    // data before user opens/changes another page/url - fetching data is asynchronous operation
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}> 
+                    {post.title}
+                </li>
+            );
+        });
+    }
+
     render() {
+        
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
-export default PostIndex;
+//when you want to consume anything from application level state you:
+function mapStateToProps(state) {
+    return { posts: state.posts };
+}
+
+//hook up the action creator to our component
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex); //instead of using mapDistpatchToProps we pass the action create directly to connect
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,13 @@
-
+import _ from 'lodash';
 
 import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
-            console.log(action.payload.data);
+            // .mapKeys is a lodash function that converts array to object
+            // it takes array as its first argument and the property that we want to pull from the abject in our array to use as a key to represent the resulting object
+            return _.mapKeys(action.payload.data, 'id');    
         default:
             return state;
     }
